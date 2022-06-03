6f37ecbc76d21262c38195ca78571c27bcb5d5c4
Philani Ncube <ncbphi001@gmail.com>
2022-06-03 14:24:44 +0200

    image links

--- data/projects.ts
+++ data/projects.ts
@@ -8,57 +8,57 @@ const projects: ProjectType[] = [
     {
         name: 'Popp Inn',
         url: 'https://poppinn.co.za',
-        image: 'https://poppinn.co.za'
+        image: '/images/poppinn.png'
 },
     {
         name: 'Hotwater24',
         url: 'https://hotwater24.com',
-        image: 'https://hotwater24.com'
+        image: '/images/hotwater24.png'
     },
     {
         name: 'Red Vapor Production',
         url: 'https://redvaporproductions.co.za',
-        image: 'https://redvaporproductions.co.za'
+        image: '/images/redvapor.png'
     },
     {
         name: 'Ink Finance',
         url: 'https://inkfinance.co.za',
-        image: 'https://inkfinance.co.za'
+        image: '/images/inkfinance.png'
     },
     {
         name: 'Fitness HQ',
         url: 'https://www.fitnesshq.co.za',
-        image: 'https://www.fitnesshq.co.za'
+        image: '/images/fitnesshq.png'
     },
     {
         name: 'Sachumas Events',
-        url: 'https://www.sachumasevents.co.za',
-        image: 'https://www.sachumasevents.co.za'
+        url: 'https://sachumasevents.com',
+        image: '/images/sachumasevents.png'
     },
     {
         name: 'ML Construction',
         url: 'https://www.maslu.co.za',
-        image: 'https://www.maslu.co.za'
+        image: '/images/maslu.png'
     },
     {
         name: 'Capegadgets',
         url: 'https://www.capegadgets.co.za',
-        image: 'https://www.capegadgets.co.za'
+        image: '/images/capegadgets.png'
     },
     {
         name: 'Skin Inc',
         url: 'https://www.skininc.co.za',
-        image: 'https://www.skininc.co.za'
+        image: '/images/skininc.png'
     },
     {
         name: 'Crafted Photographs',
-        url: 'https://www.craftedphotographs.co.za',
-        image: 'https://www.craftedphotographs.co.za'
+        url: 'https://www.craftedphotographs.com',
+        image: '/images/craftedphotographs.png'
     },
     {
         name: 'Iris Academy',
         url: 'https://www.leonorahirisacademy.co.za',
-        image: 'https://www.leonorahirisacademy.co.za'
+        image: '/images/irisacademy.png'
     },
     
 ];
