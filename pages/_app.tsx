2e5c409662c4e54888a35046cb7bbb14a888ab72
Philani Ncube <ncbphi001@gmail.com>
2022-06-04 22:06:37 +0200

    footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,21 @@ import { Fragment } from "react";
 import type { AppProps } from 'next/app'
 import Header from '../components/Header';
 import MenuManager from '../components/Menu/MenuManager';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
+
   return (
     <Fragment>
         <MenuManager >
             <Header  />
             <Component {...pageProps} />
+            <Footer />
         </MenuManager>
     </Fragment>
   )
 }
-      
-  
+
+
 
 export default MyApp
